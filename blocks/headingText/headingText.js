4ea5badabb7948a7b1bbacc13a2698e734e824aa
kalpesheds <kalpeshpurohitt@gmail.com>
2025-03-14 14:14:30 +0530

    Update headingText.js

--- blocks/headingText/headingText.js
+++ blocks/headingText/headingText.js
@@ -1,1 +1,22 @@
 
+export default function decorate(block) {
+    block.classList.add('content-block-ddd');
+    
+    [...block.children].forEach((row) => {
+     
+        const heading = row.querySelector('h2, h3, h4, h5, h6');
+        const description = row.querySelector('p');
+        const button = row.querySelector('a, button');
+        
+        if (heading) {
+          heading.classList.add('content-heading-ddd');
+        }
+        if (description) {
+          description.classList.add('content-description-ddd');
+        }
+        if (button) {
+          button.classList.add('content-button-ddd');
+        }
+      
+    });
+  }
