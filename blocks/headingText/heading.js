c6e30e928d64f8da860099577d69e8034b57ddd0
kalpeshrajpurohit <kandyrajpurohit@gmail.com>
2025-03-14 13:50:36 +0530

    css

---
+++ blocks/headingText/heading.js
@@ -0,0 +1,21 @@
+export default function decorate(block) {
+    block.classList.add('content-block');
+    
+    [...block.children].forEach((row) => {
+      [...row.children].forEach((col) => {
+        const heading = col.querySelector('h2, h3, h4, h5, h6');
+        const description = col.querySelector('p');
+        const button = col.querySelector('a, button');
+        
+        if (heading) {
+          heading.classList.add('content-heading');
+        }
+        if (description) {
+          description.classList.add('content-description');
+        }
+        if (button) {
+          button.classList.add('content-button');
+        }
+      });
+    });
+  }
