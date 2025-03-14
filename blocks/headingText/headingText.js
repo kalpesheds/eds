4851adb809dddacb2e2b52ce18c8214a8fa7bf38
kalpesheds <kalpeshpurohitt@gmail.com>
2025-03-14 14:12:56 +0530

    Update headingText.js

--- blocks/headingText/headingText.js
+++ blocks/headingText/headingText.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-    block.classList.add('content-block-rajpurohit');
+    block.classList.add('content-block-ddd');
     
     [...block.children].forEach((row) => {
       [...row.children].forEach((col) => {
@@ -8,13 +8,13 @@ export default function decorate(block) {
         const button = col.querySelector('a, button');
         
         if (heading) {
-          heading.classList.add('content-heading');
+          heading.classList.add('content-heading-ddd');
         }
         if (description) {
-          description.classList.add('content-description');
+          description.classList.add('content-description-ddd');
         }
         if (button) {
-          button.classList.add('content-button');
+          button.classList.add('content-button-ddd');
         }
       });
     });
